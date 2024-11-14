ad9dfb1cf65cd52b366dc2428ec81e4a7fdf4db5
Gonzalo Rosofsky <156929546+gonzalorosofsky@users.noreply.github.com>
2024-11-14 14:27:21 -0300

    validateRegister refactoring

--- src/interfaces/middlewares/validateRegister.ts
+++ src/interfaces/middlewares/validateRegister.ts
@@ -5,28 +5,21 @@ import { check, ValidationChain } from "express-validator";
 
 const validateRegister: ValidationChain[] = [
   check("firstName")
-    .notEmpty().withMessage("Debe ingresar un nombre.")
-    .isLength({ min: 2, max: 20 }).withMessage("El nombre ingresado debe tener entre 2 y 20 letras."),
-
+  .trim()
+  .notEmpty().withMessage('Debe ingresar su nombre')
+  .matches(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/).withMessage('El nombre solo puede contener letras')
+  .isLength({ min: 2, max: 50 }).withMessage('El nombre ingresado debe tener entre 2 y 50 letras.'),
+  
   check("lastName")
+    .trim()
     .notEmpty().withMessage("Debe ingresar un apellido.")
-    .isLength({ min: 2, max: 20 }).withMessage("El apellido ingresado debe tener entre 2 y 20 letras."),
-
-  check("username")
-    .notEmpty().withMessage("Debe ingresar un nombre de usuario.")
-    .isLength({ min: 5, max: 15 }).withMessage("El nombre de usuario ingresado debe tener entre 5 y 15 letras.")
-    // .custom(async (value) => {
-    //   const user = await db.User.findOne({ where: { username: value } });
-    //   if (!user) {
-    //     return true;
-    //   }
-    //   throw new Error("El nombre de usuario ingresado ya está en uso.");
-    // })
-    ,
+    .isLength({ min: 2, max: 20 }).withMessage("El apellido ingresado debe tener entre 2 y 20 letras.")
+    .matches(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/).withMessage('El apellido solo puede contener letras')
+    .isLength({ min: 2, max: 50 }).withMessage('El apellido ingresado debe tener entre 2 y 50 letras.'),
 
   check("email")
     .notEmpty().withMessage("Debe ingresar un mail.")
-    .isEmail().withMessage("Debe ingresar un mail válido.")
+    .isEmail().withMessage('Debe ser un correo electrónico válido').normalizeEmail(),
     // .custom(async (value) => {
     //   const user = await db.User.findOne({ where: { email: value } });
     //   if (!user) {
@@ -34,11 +27,10 @@ const validateRegister: ValidationChain[] = [
     //   }
     //   throw new Error("El mail ingresado ya está en uso.");
     // })
-    ,
 
   check("password")
     .notEmpty().withMessage("Debe ingresar una contraseña.")
-    .isLength({ min: 8, max: 25 }).withMessage("La contraseña ingresada debe tener entre 8 y 25 letras.")
+    .isLength({ min: 8, max: 25 }).withMessage("La contraseña ingresada debe tener entre 8 y 25 caracteres.")
     .custom((value) => {
       if (!(/[A-Z]/.test(value))) {
         throw new Error("La contraseña ingresada debe contener mayúsculas.");
@@ -56,9 +48,23 @@ const validateRegister: ValidationChain[] = [
         throw new Error("La contraseña ingresada debe contener números y letras.");
       }
       return true;
+    })
+    .custom((value) => {
+        if (!(/[@$!%*?&]/.test(value))) {
+            throw new Error("La contraseña ingresada debe contener un caracter especial.");
+          }
+          return true;
     }),
 
-//   check("image")
+    check("confirm-password")
+    .custom((value, { req }) => {
+      if (value === req.body.password) {
+        return true;
+      }
+      throw new Error("Las contraseñas no coinciden.");
+    })
+
+//   check("img")
 //     .custom((value, { req }) => {
 //       const file = (req as Request).file;
 //       if (file && ["image/png", "image/jpeg", "image/jpg", "image/gif"].includes(file.mimetype.toLowerCase())) {
@@ -67,13 +73,7 @@ const validateRegister: ValidationChain[] = [
 //       throw new Error("La imagen debe ser en formato PNG, JPEG, JPG o GIF.");
 //     }),
 
-  check("confirm-password")
-    .custom((value, { req }) => {
-      if (value === req.body.password) {
-        return true;
-      }
-      throw new Error("Las contraseñas no coinciden.");
-    })
+
 ];
 
 export default validateRegister;
