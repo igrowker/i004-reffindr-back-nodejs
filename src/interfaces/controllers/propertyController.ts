caa77cc500e9811996dca6312990613bf3961e59
Gonzalo Rosofsky <156929546+gonzalorosofsky@users.noreply.github.com>
2024-11-15 12:56:09 -0300

    selectCandidates method added to propertyController

--- src/interfaces/controllers/propertyController.ts
+++ src/interfaces/controllers/propertyController.ts
@@ -10,15 +10,21 @@ export const getProperties = async (req: Request, res: Response) => {
 }
 
 export const getPropertyById = async (req: Request, res: Response) => {
-      const { id } = req.params;
-      const response = await httpClient.get(`${BACKEND_URL}/properties/${id}`);
-      res.json(response.data);
+      const { id } = req.params
+      const response = await httpClient.get(`${BACKEND_URL}/properties/${id}`)
+      res.json(response.data)
   };
   
 
 export const createProperty = async (req: Request, res: Response) => {
     const response = await httpClient.post(`${BACKEND_URL}/properties`, req.body)
     res.status(201).json(response.data)
+}
+
+export const selectCandidates = async (req: Request, res: Response) => {
+      const { propertyId } = req.params
+      const response = await httpClient.post(`${BACKEND_URL}/select-candidates/${propertyId}`, req.body)
+      res.status(200).json(response.data)
 }
 
 export default router
