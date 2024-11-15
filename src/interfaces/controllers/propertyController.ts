ec2de627bccb277bdda9d95085613e2372250d8f
Gonzalo Rosofsky <156929546+gonzalorosofsky@users.noreply.github.com>
2024-11-15 11:59:52 -0300

    propertyController added to controllers

---
+++ src/interfaces/controllers/propertyController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express'
+import axios from 'axios'
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export const getProperties = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/properties`, { params: req.query })
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener propiedades.' })
+  }
+}
+
+export const createProperty = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/properties`, req.body)
+    res.status(201).json(response.data)
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear la propiedad.' })
+  }
+}
