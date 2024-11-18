b1eb7fbff14ae301a7c6aa35e57de3e4074e827b
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-11-18 18:26:45 +0100

    Refactor authController to streamline registration and add login endpoint

--- src/interfaces/controllers/authController.ts
+++ src/interfaces/controllers/authController.ts
@@ -5,31 +5,45 @@ import validateRegister from '../middlewares/validateRegister'
 
 const router = Router()
 
-router.post(
-  '/register', validateRegister ,
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
-    }
-
-    const { name, surname, email, password } = req.body
-
-    try {
-      const response = await httpClient.post('/users/register', {
-        name,
-        surname,
-        email,
-        password,
-      })
-
-      return res.status(response.status).json(response.data)
-    } catch (error: any) {
-      return res
-        .status(error.response?.status || 500)
-        .json({ error: error.response?.data || 'Error interno del servidor' })
-    }
+router.post('/register', validateRegister, async (req: Request, res: Response) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
   }
-)
+
+  const { name, surname, email, password } = req.body
+
+  try {
+    const response = await httpClient.post('/users/register', {
+      name,
+      surname,
+      email,
+      password,
+    })
+
+    return res.status(response.status).json(response.data)
+  } catch (error: any) {
+    return res
+      .status(error.response?.status || 500)
+      .json({ error: error.response?.data || 'Error interno del servidor' })
+  }
+})
+
+router.post('/login', async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const response = await httpClient.post('/users/login', {
+      email,
+      password,
+    })
+
+    return res.status(response.status).json(response.data)
+  } catch (error: any) {
+    return res
+      .status(error.response?.status || 500)
+      .json({ error: error.response?.data || 'Error interno del servidor' })
+  }
+})
 
 export default router
