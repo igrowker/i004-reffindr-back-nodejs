6e82442aa0635831a2176608a6fa533181346204
j0sep0z0 <j0sep0z0@outlook.com>
2024-11-25 23:24:05 +0100

    Refactor property creation endpoint to streamline request handling and improve error messaging; update HTTP client configuration

--- src/interfaces/controllers/propertyController.ts
+++ src/interfaces/controllers/propertyController.ts
@@ -2,14 +2,117 @@ import { Request, Response, Router } from 'express'
 import { validationResult } from 'express-validator'
 
 import { BaseResponse } from '../../shared/utils/baseResponse'
-import validateCreateProperty from '../middlewares/validateCreateProperty'
+// import validateCreateProperty from '../middlewares/validateCreateProperty'
 import httpClient from '../services/httpClient'
 
 const router = Router()
 
-router.post('/createProperty', validateCreateProperty, async (req: Request, res: Response) => {
-  const errors = validationResult(req)
+// router.post('/createProperty', validateCreateProperty, async (req: Request, res: Response) => {
+//   const errors = validationResult(req)
 
+//   if (!errors.isEmpty()) {
+//     const errorsValidation = errors.array().map((error) => error.msg)
+
+//     return res.status(400).json(
+//       new BaseResponse({
+//         errors: errorsValidation,
+//         hasErrors: true,
+//         statusCode: res.statusCode,
+//       })
+//     )
+//   }
+
+//   const { ownerEmail } = req.query
+
+//   if (!ownerEmail || typeof ownerEmail !== 'string') {
+//     return res.status(400).json(
+//       new BaseResponse({
+//         errors: ['El correo del propietario (ownerEmail) es obligatorio y debe ser un string.'],
+//         hasErrors: true,
+//         statusCode: res.statusCode,
+//       })
+//     )
+//   }
+
+//   const {
+//     countryId,
+//     stateId,
+//     title,
+//     address,
+//     environments,
+//     bathrooms,
+//     bedrooms,
+//     seniority,
+//     water,
+//     gas,
+//     surveillance,
+//     electricity,
+//     internet,
+//     pool,
+//     garage,
+//     pets,
+//     grill,
+//     elevator,
+//     terrace,
+//     description,
+//     requirementPostRequestDto: { isWorking, hasWarranty, rangeSalary },
+//   } = req.body
+
+//   const token = req.headers.authorization
+
+//   try {
+//     const response = await httpClient.post(
+//       '/Properties/PostProperty',
+//       {
+//         CountryId: countryId,
+//         StateId: stateId,
+//         Title: title,
+//         Address: address,
+//         Environments: environments,
+//         Bathrooms: bathrooms,
+//         Bedrooms: bedrooms,
+//         Seniority: seniority,
+//         Water: water,
+//         Gas: gas,
+//         Surveillance: surveillance,
+//         Electricity: electricity,
+//         Internet: internet,
+//         Pool: pool,
+//         Garage: garage,
+//         Pets: pets,
+//         Grill: grill,
+//         Elevator: elevator,
+//         Terrace: terrace,
+//         Description: description,
+//         RequirementPostRequestDto: {
+//           IsWorking: isWorking,
+//           HasWarranty: hasWarranty,
+//           RangeSalary: rangeSalary,
+//         },
+//         OwnerEmail: ownerEmail,
+//       },
+//       {
+//         headers: {
+//           Authorization: token, // Pasar el token en el encabezado de la solicitud
+//         },
+//       }
+//     )
+
+//     return res.status(response.status).json(response.data)
+//   } catch (error: unknown) {
+//     console.error(error)
+//     return res.status(400).json(
+//       new BaseResponse({
+//         errors: ['Error al registrar la propiedad.'],
+//         hasErrors: true,
+//         statusCode: res.statusCode,
+//       })
+//     )
+//   }
+// })
+
+router.post('/createProperty', async (req: Request, res: Response) => {
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
     const errorsValidation = errors.array().map((error) => error.msg)
 
@@ -23,76 +126,18 @@ router.post('/createProperty', validateCreateProperty, async (req: Request, res:
   }
 
   const { ownerEmail } = req.query
-
-  if (!ownerEmail || typeof ownerEmail !== 'string') {
-    return res.status(400).json(
-      new BaseResponse({
-        errors: ['El correo del propietario (ownerEmail) es obligatorio y debe ser un string.'],
-        hasErrors: true,
-        statusCode: res.statusCode,
-      })
-    )
-  }
-
-  const {
-    countryId,
-    stateId,
-    title,
-    address,
-    environments,
-    bathrooms,
-    bedrooms,
-    seniority,
-    water,
-    gas,
-    surveillance,
-    electricity,
-    internet,
-    pool,
-    garage,
-    pets,
-    grill,
-    elevator,
-    terrace,
-    description,
-    requirementPostRequestDto: { isWorking, hasWarranty, rangeSalary },
-  } = req.body
+  const propertyPostRequestDto = req.body
 
   try {
-    const response = await httpClient.post('/Properties/PostProperty/', {
-      CountryId: countryId,
-      StateId: stateId,
-      Title: title,
-      Address: address,
-      Environments: environments,
-      Bathrooms: bathrooms,
-      Bedrooms: bedrooms,
-      Seniority: seniority,
-      Water: water,
-      Gas: gas,
-      Surveillance: surveillance,
-      Electricity: electricity,
-      Internet: internet,
-      Pool: pool,
-      Garage: garage,
-      Pets: pets,
-      Grill: grill,
-      Elevator: elevator,
-      Terrace: terrace,
-      Description: description,
-      RequirementPostRequestDto: {
-        IsWorking: isWorking,
-        HasWarranty: hasWarranty,
-        RangeSalary: rangeSalary,
-      },
-      OwnerEmail: ownerEmail,
+    const response = await httpClient.post('/Properties/PostProperty', propertyPostRequestDto, {
+      params: { ownerEmail },
     })
 
     return res.status(response.status).json(response.data)
   } catch (error: unknown) {
     return res.status(400).json(
       new BaseResponse({
-        errors: ['Error al registrar la propiedad.'],
+        errors: ['Error al publicar la propiedad.'],
         hasErrors: true,
         statusCode: res.statusCode,
       })
--- src/interfaces/services/httpClient.ts
+++ src/interfaces/services/httpClient.ts
@@ -1,23 +1,22 @@
 import axios from 'axios'
-import https from 'https';
 import 'dotenv/config'
+import https from 'https'
 
 const { BACKEND_URL } = process.env
 
 const agent = new https.Agent({
   rejectUnauthorized: false,
-});
+})
 
 const httpClient = axios.create({
   baseURL: BACKEND_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsIm5hbWUiOiJzdHJpbmciLCJzdWIiOiIxIiwicm9sZSI6IjEiLCJuYmYiOjE3MzI1Njk5MDgsImV4cCI6MTczMzAwMTkwOCwiaWF0IjoxNzMyNTY5OTA4LCJpc3MiOiJBdXRoSXNzdWVyIiwiYXVkIjoiVXNlcnNBdWRpZW5jZSJ9.aFHul-J0sAh_gGRJm_iu7DoXzVLazsgH8TPJ3SVirc15vAuWGPxIvur4968t5PaROoOL4SYfToOAfEdcFesYog`,
   },
   httpsAgent: agent,
-
 })
-
 
 httpClient.interceptors.response.use(
   (response) => response,
@@ -25,11 +24,10 @@ httpClient.interceptors.response.use(
     return Promise.reject({
       response: {
         status: error.response?.status || 500,
-        data: error.response?.data || { message: "Internal Server Error." },
+        data: error.response?.data || { message: 'Internal Server Error.' },
       },
-      
-    });
+    })
   }
-);
+)
 
 export default httpClient
