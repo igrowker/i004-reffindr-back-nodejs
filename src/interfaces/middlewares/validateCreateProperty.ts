1b7b11d43c1c98a0c0923f0d50cc6c6559ddde60
Jose Pozo <145009592+j0sep0z0@users.noreply.github.com>
2024-11-25 15:36:07 +0100

    Refactor import statements for consistency, enhance error handling in property creation, and add validation middleware for property creation (#54)

---
+++ src/interfaces/middlewares/validateCreateProperty.ts
@@ -0,0 +1,38 @@
+import { check, ValidationChain } from 'express-validator'
+
+const validateCreateProperty: ValidationChain[] = [
+  check('countryId').notEmpty().withMessage('Debe seleccionar un país'),
+  check('stateId').notEmpty().withMessage('Debe seleccionar una provincia'),
+  check('title')
+    .notEmpty()
+    .withMessage('Debe ingresar un nombre de propiedad')
+    .isString()
+    .withMessage('Debe ingresar un nombre de propiedad válido')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('El nopmbre de la propiedad debe tener enrtre 2 y 50 letras.'),
+  check('address')
+    .notEmpty()
+    .withMessage('Debe ingresar una dirección')
+    .isString()
+    .withMessage('Debe ingresar una dirección válida.')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('La dirección ingresada debe tener enrtre 2 y 50 letras.'),
+  check('environments').notEmpty().withMessage('Debe seleccionar un número de ambientes'),
+  check('bathrooms').notEmpty().withMessage('Debe seleccionar un número de baños'),
+  check('bedrooms').notEmpty().withMessage('Debe seleccionar un número de habitaciones'),
+  check('seniority').notEmpty().withMessage('Debe seleccionar un año de antiguedad'),
+  check('description')
+    .notEmpty()
+    .withMessage('Debe ingresar una descripción')
+    .isString()
+    .withMessage('Debe ingresar una descripción válida.')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('La descripción ingresada debe tener enrtre 2 y 50 letras.'),
+  check('requirementPostRequestDto.rangeSalary')
+    .notEmpty()
+    .withMessage('Debe seleccionar un rango salarial')
+    .isNumeric()
+    .withMessage('Debe seleccionar un número.'),
+]
+
+export default validateCreateProperty
